0cc3c01f86dddea9c12ef614ef8793c2cf17b2b0
Éder Amadeu <eder.amadeu@gmail.com>
2023-01-10 20:00:05 -0300

    commit atualizaJS2

--- JS/script.js
+++ JS/script.js
@@ -44,3 +44,22 @@ function eventoPerguntas(pergunta) {
 }
 
 perguntas.forEach(eventoPerguntas);
+
+// galeria de bicicletas//
+
+const galeria = document.querySelectorAll(".bicicletas-imagens img");
+const galeriaContainer = document.querySelector(".bicicletas-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function EventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(EventosGaleria);
