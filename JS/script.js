94f9a1acf5f7a6d58708f0cbd113b489a240688a
Éder Amadeu <eder.amadeu@gmail.com>
2023-01-10 19:35:04 -0300

    commit atualizaJS2

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,5 @@
+//ativar lnks do menu home//
+
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
@@ -9,3 +11,36 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+// ativar itens do orçamento de bicicletas e seguros//
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
+
+//automação js com perguntas frequentes//
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventoPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventoPerguntas);
